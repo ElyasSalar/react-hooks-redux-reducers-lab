bd1b37669672ca5942f045b42e193f85be75b86b
Elyas <elyassalar8@gmail.com>
2021-08-29 10:30:49 +0300

    41th assignment solution

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state={friends: []}, action) {
+  switch(action.type){
+    case 'friends/add':
+      return {
+        friends: [
+          ...state.friends,
+          action.payload
+        ]
+      }
+    case 'friends/remove':
+      state.friends.forEach((friend, index) => {
+        if(friend.id === action.payload) state.friends.splice(index, 1);
+      })
+      return state;
+    default:
+      return state;
+  }
 }
